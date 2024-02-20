e991092d62c06bb9d59925d4c6dea505964b5518
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-20 01:41:04 +0000

    c

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int c;
+	float d;
+	pritnf("Size of a char: %lu byte(s)\n", sizeof(char));
+	pritnf("Size of a int: %lu byte(s)\n", sizeof(int));
+	pritnf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	pritnf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	pritnf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
