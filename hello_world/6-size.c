34d836a9cf82a3fcb2e512a55fff485d4f41ddea
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-20 01:52:06 +0000

    c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	pritnf("Size of a char: %lu byte(s)\n", sizeof(char));
-	pritnf("Size of a int: %lu byte(s)\n", sizeof(int));
-	pritnf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	pritnf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	pritnf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
