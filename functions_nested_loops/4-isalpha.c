e26e5abaebccefc13ab0ce63d21a3def6b9c0c55
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-23 23:46:57 +0000

    c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: An input character
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _isalpha(int c)
+{
+	char a;
+	char b;
+	int value = 0;
+
+	for (a = 'a', b = 'A'; a <= 'z' && b <= 'Z'; a++, b++)
+	{
+		if (a == c || b==c)
+		{
+			value = 1;
+		}
+	}
+
+	return (value);
+}
