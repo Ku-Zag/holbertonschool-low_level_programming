07c5256235a6ca7d22f5b943dc19c18739a783a9
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-23 00:18:58 +0000

    c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{	
+	for (char c = 'a'; char c <= 'z'; char c++)
+	{
+		_putchar(char c);
+	}
+
+	return (0);
+}
