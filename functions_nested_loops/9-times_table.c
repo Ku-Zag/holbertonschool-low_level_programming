082007a7c9f8984b99691b656b081c050924039d
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-24 23:25:01 +0000

    c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,24 +22,22 @@ void times_table(void)
 			if (c < 10)
 			{
 				_putchar(c + '0');
-				if (b == 9)
+				if (b != 9)
 				{
-					_putchar(' ');
-				}
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				if ( b == 9)
+				if ( b != 9)
 				{
-					_putchar(' ');
-				}
 				_putchar(',');
 				_putchar(' ');
+				}
 			}
 			b++;
 		}
