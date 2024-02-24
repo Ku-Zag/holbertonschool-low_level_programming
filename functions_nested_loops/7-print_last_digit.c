b5bf03f4a22f0923181b5fa11c3fe00ffb81523c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-24 20:07:10 +0000

    c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: interger
+ * Description: find last digit
+ * Return: last digit
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+	if (n >= 0)
+	{
+		ld = n%10;
+	}
+	else if (n < 0)
+	{
+		ld = -n%10;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
+
