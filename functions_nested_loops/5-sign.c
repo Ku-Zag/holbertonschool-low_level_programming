309c80f0aa0a7e7a8691e9a7724258368a60310c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-24 19:18:49 +0000

    c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Return: 0 
+ */
+
+int print_sign(int n)
+{
+	int value = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		value = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		value = -1;
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (value);
+}
