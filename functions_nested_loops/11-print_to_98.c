1b6d228d8e61e77170edc5271dca59cce538df25
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-25 00:47:26 +0000

    c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			
+
 			if (n == 98)
 				continue;
 			printf(", ");
 		}
 		printf("\n");
-	}	
+
 	else if (n >= 98)
 	{
 		for (; n >= 98; n--)
