a0e0a3ceaf9b1a8fe6c8dc82a09b3608f5f7a7b8
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-25 00:05:18 +0000

    c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,6 +16,7 @@ void times_table(void)
 	while (a <= 9)
 	{
 		b = 0;
+
 		while(b <= 9)
 		{
 			c = a * b;
@@ -37,6 +38,7 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+
 				if ( b != 9)
 				{
 				_putchar(',');
