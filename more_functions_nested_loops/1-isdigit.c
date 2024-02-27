6c9c0b403f590b14ef891d17cd7ef9ff51056b77
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-27 15:38:54 +0000

    more functions

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isdigit - entry
+ * @c: value
+ * Description - _isdigit returns 1 if char is digit, 0 otherwise
+ * Return: value
+ */
+int _isdigit(int c)
+{
+	int n;
+	int value = 0;
+
+	for (n ='0'; n <= '9'; n++)
+	{
+		if (c == n)
+		{
+			value = 1;
+		}
+	}
+	return (value);
+}
