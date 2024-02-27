8ded6c9cac75e7d773afb59fe5c68e8c430b1228
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-27 15:47:26 +0000

    more functions

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - entry
+ * @a - interger
+ * @b - integer
+ * Description: mul mulyiplies integers
+ * Return - value
+ */
+int mul(int a, int b)
+{
+	int value;
+	value = a * b;
+	return(value);
+}
