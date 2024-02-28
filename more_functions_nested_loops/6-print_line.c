a7d1050d7771c03bd0849f6c2e8adf991fa599e6
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-28 17:01:59 +0000

    more functions

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,17 @@
 #include "main.h"
 /**
+ * print_line - entry
+ * @n: integer
+ * Description: print_line prints n line
+ * Return: void
  */
 void print_line(int n)
 {
 	int a;
 
-	for ()
+	for(a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
