1baa0dc4d2a3a9e0c83830cca0bee45f17efc346
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-28 18:04:54 +0000

    more functions

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diogonal - entry
+ * @n: integer
+ * Description: prints diognal
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int s;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (s = 0; s < a; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
