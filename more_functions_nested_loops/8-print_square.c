d95f44bc9c29a69696f4d0d943496efda9b166b3
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-28 20:10:50 +0000

    more functions

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,22 @@
  */
 void print_square(int size)
 {
-        int l;
-        int w;
+	int l;
+	int w;
 
-        if (size > 0)
-        {
-                for (l = 0; l < size; l++)
-                {
-                        for (w = 0; w < size; w++)
-                        {
+	if (size > 0)
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (w = 0; w < size; w++)
+			{
 				_putchar('#');
 			}
 			_putchar('\n');
-                }
-        }
-        else
-        {
-                _putchar('\n');
-        }
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
