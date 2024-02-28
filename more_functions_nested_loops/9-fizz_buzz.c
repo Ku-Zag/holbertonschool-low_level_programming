9f8f6497c740a43d4c00cdb2eeae648678663d87
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-28 20:31:52 +0000

    more functions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description: main Fizz Buzz
+ * Return: void
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 0; n <=100; n++)
+	{
+		if (n != 100)
+		{
+			if (n % 3 == 0 && n % 5 == 0)
+			{
+				printf("FizzBuzz ");
+				continue;
+			}
+			if (n % 3 == 0)
+				printf("Fizz ");
+			else if (n % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", n);
+		}
+		else
+			printf("FizzBuzz");
+	}
+	putchar('\n');
+}
