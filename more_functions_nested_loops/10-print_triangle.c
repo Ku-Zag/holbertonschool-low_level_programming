4f07a1a277fb2b446ab371af802dddcb08e5bc57
Ku-Zag <abdullamcidov2003@gmail.com>
2024-02-28 23:20:19 +0000

    more functions

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - entry
+ * @size: size
+ * Description: print_triangle 
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size - a; b++)
+				_putchar(' ');
+
+			for (b = 0; b <= a; b++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
