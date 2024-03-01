7e3585400a4e6050bd41ee2c6c52520ed4c56b6c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-01 21:12:13 +0000

    pointer

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int i = 0;
+	char *c = dest;
+
+	while (src[l] != 0)
+		l++;
+
+	while (i < l)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (c);
+}
