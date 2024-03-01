7233af924ab4ed62889763547e1dcbc5786fdd3f
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-01 13:18:52 +0000

    pointer

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - entry
+ * @s: string
+ * Description: rev_string revs string
+ */
+void rev_string(char *s)
+{
+	char c;
+	int n = 0, l = 0;
+
+	while (s[l] != 0)
+		l++;
+	while (n < l)
+	{
+		c = s[n];
+		s[n] = s[l-1];
+		s[l-1] = c;
+		n++;
+		l--;
+	}
+}
