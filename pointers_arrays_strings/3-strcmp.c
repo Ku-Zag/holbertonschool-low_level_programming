df2a21bbd4edd93f162855f416502277ddde6400
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-02 23:01:27 +0000

    pointer

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - entry
+ * @s1: s1
+ * @s2: s2
+ * Description: _strcmp compares
+ * Return: interge
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int differ; 
+
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	differ = *s1 - *s2;
+	return (differ);
+}
