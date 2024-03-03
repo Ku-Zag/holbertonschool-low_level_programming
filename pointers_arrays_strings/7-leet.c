0e8b023f677f9b783027e6d998032f68cb639ea3
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-03 01:52:04 +0000

    pointer

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -5,16 +5,17 @@
  * Description: leet is 1337
  * Return: char
  */
-char *leet(char c)
+char *leet(char *c)
 {
-	int origin[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'}
-	int change[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'}
-	char *p = c;
+	int origin[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int change[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i = 0;
-	int l = 0;
+	int l;
 
 	while (c[i])
 	{
+		l = 0;
+
 		while (l < 10)
 		{
 			if (c[i] == origin[l])
@@ -23,5 +24,5 @@ char *leet(char c)
 		}
 		i++;
 	}
-	return (p);
+	return (c);
 }
