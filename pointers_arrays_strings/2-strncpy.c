db1dda77de8bf384aeb0022536d669141023cbc7
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-03 02:04:47 +0000

    pointer

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
+ * _strncpy - entry
+ * @dest: des
+ * @src: source
+ * @n: integer
+ * Description: _strncpy hate
+ * Return: char
  */
-char *_strncpy(char *dest, char *src, int n) {
-    int i;
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-    for (; i < n; i++) {
-        dest[i] = '\0';
-    }
+	for (; i < n; i++)
+		dest[i] = '\0';
 
-    return dest;
+	return dest;
 }
-
