284d3cab92e556bdad91d47ec4c58312914721f1
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-05 10:51:40 +0000

    enough

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
+ * _memset - entry
+ * @s: strrig
+ * @b: char
+ * @n: integer
+ * Description: _memset do what memset() does
+ * Return: char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int l = 0;
 	char *ptr = s;
-	
+
 	while (l < n)
 	{
 		ptr[l] = b;
