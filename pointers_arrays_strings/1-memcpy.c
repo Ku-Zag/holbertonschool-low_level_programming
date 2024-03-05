11cf98dd2fc978d1a70d104a91d433b8e29c1829
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-05 11:29:55 +0000

    enough

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - entry
+ * @dest: destination
+ * @src: source
+ * @n: intger
+ * Description: _memcpy = memcpy
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int l = 0;
+	char *pd = dest;
+	
+	while (l < n)
+	{
+		pd[l] = src[l];
+		l++;
+	}
+	return (pd);
+}
