3aa08937d19aebd24354abd1a6608c73d18c8c73
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-07 15:35:51 +0000

    enough

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,20 @@
 #include "main.h"
 /**
+ * _strspn - emtry
+ * @s: string
+ * @accepy: accpet
+ * Description: _strspn
+ * Return: unsig. char
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	char *p = accept;
 	unsigned int l = 0;
-	unsigned int n = 0;
 
 	while (*s)
 	{
-		
+		accept = p;
+
 		while  (*accept)
 		{
 			if (*s == *accept)
@@ -18,7 +24,10 @@ unsigned int _strspn(char *s, char *accept)
 			}
 			accept++;
 		}
-		accept++;
+
+		if (*accept == 0)
+			break;
+		s++;
 	}
 	return (l);
 
