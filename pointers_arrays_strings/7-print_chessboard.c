08511a84fe19bb4ede841d7a351f07fad7c0ee97
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-07 20:13:49 +0000

    enough

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
+ * print_chessboard - enrty
+ * @a: string
+ * Description: print_chessboard
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
+	int l = 0;
+	int n;
 
+	while (l < 8)
+	{
+		n = 0;
+		while (n < 8)
+		{
+			_putchar(a[l][n]);
+			n++;
+		}
+		l++;
+		_putchar('\n');
+	}
 }
