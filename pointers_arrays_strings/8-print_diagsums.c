ce4d1418bba166362ce2e8f1238c2b2ac9ab1148
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-07 20:56:28 +0000

    enough

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ */
+void print_diagsums(int *a, int size)
+{
+	int l = 0;
+	int n = 0;
+	unsigned long int sum1 = 0;
+	unsigned long int sum2 = 0;
+
+	while (l < size && n < size)
+	{
+		sum1 += a[l * size + n];
+		n++;
+		l++;
+		sum2 += a[l * size - n];
+	}
+	printf("%lu, %lu\n", sum1, sum2);
+}
