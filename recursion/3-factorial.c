353d769506afbd77d495c7757f8ed5cfebe345b8
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-08 20:44:24 +0000

    recursion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - entry
+ * @n: integegr
+ * Description: find factorial
+ * Return: Integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return 1;
+
+	return (n * factorial(n-1));
+}
