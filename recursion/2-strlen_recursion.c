32ada273ff550a3e586bb32426e7f1f5ef7fff8b
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-08 20:39:09 +0000

    recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion - entry
  * @s: string
- * Description: _strlen_recursion helps 
+ * Description: _strlen_recursion helps
  * Return: integer
  */
 int _strlen_recursion(char *s)
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s)
 	if (*s == 0)
 		return (0);
 
-	return (1 +_strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
