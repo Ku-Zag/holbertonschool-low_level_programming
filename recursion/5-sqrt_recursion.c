283d39ee2a724e9dff56570b92b32c08dcc8d37c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-08 22:33:13 +0000

    recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - entru
+ * @n: integer
+ * Description: sqrt ffinder
+ * Return: integer
+ */
+int find_sqrt(int l, int n);
+
+int find_sqrt(int l, int n)
+{
+	if (l > n)
+		return (-1);
+
+	else if (n == l * l)
+		return (l);
+
+	return (find_sqrt(l + 1, n));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	
+	return (find_sqrt(1, n));
+}
