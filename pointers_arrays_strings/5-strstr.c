246c1bdec4081a4cb90a749a079c0a750e75b671
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-08 12:27:27 +0000

    enough

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
  */
 char *_strstr(char *haystack, char *needle)
 {
+	char *ph = haystack;
+	char *pn = needle;
 
+	while (*haystack)
+	{
+		needle = pn;
+		haystack = ph;
+
+		while (*needle != 0 && *haystack == *needle)
+		{
+			needle++;
+			haystack++;
+		}
+
+		if (*needle == 0)
+			return (ph);
+		ph++;
+	}
+	return (NULL);
 }
