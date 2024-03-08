6c82d1b88bf42ec60b6c780dc6832309ea828492
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-08 22:59:05 +0000

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,18 +1,23 @@
 #include "main.h"
 /**
  */
+int find_prime(int l, int n);
+
 int is_prime_number(int n)
 {
-	if (n = 0)
+	if (n == 1)
+	       return (0);
+
+	return (find_prime(2, n));
 }
 
 int find_prime(int l, int n)
 {
-	if (l == n )
-		return (0);
+	if (n % l != 0)
+		return (1);
 
 	else if (n % l == 0)
-		return (1);
+		return (0);
 
 	return (find_prime(l + 1, n));
 
