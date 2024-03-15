3c3f18fec28b880b3a91fc2fad8df3364dcd2629
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-15 00:09:35 +0000

    mal lock

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
 /**
+ * create_array - entry
+ * @size: size of array
+ * @c: certain char
+ * Description: creates array
+ * Return: char
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *ptr;
 
+	ptr = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
 }
