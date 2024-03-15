e16118fc2bba43f032dcadcedb1d1e9696608c70
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-15 00:25:40 +0000

    mal lock

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,1 +1,23 @@
 #include "main.h"
+/**
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0;
+	int l = 0;
+
+	l = strlen(str);
+	ptr = malloc(sizeof(char) * l);
+	
+	if (l == 0);
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < l)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+}
