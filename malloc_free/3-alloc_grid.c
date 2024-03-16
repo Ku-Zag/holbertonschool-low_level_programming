bde9d1ab180c04c29afc090296ea66aa2c33e199
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 17:51:48 +0000

    mal lock

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	d = (int**)malloc(sizeof(int *) * height);
+	d = (int **)malloc(sizeof(int *) * height);
 
 	if (d == NULL)
 	{
