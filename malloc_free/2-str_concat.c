f6207861919ba4c2512770d9bc65daa52c1c38a1
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 15:05:20 +0000

    mal lock

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,10 +8,11 @@ char *str_concat(char *s1, char *s2)
 	int l1 = 0;
 	int l2 = 0;
 	int l3 = 0;
+
 	if (s1 == NULL)
-		*s1 = 0;
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = 0;
+		s2 = "";
 	while (s1[l1] != 0)
 		l1++;
 	while (s2[l2] != 0)
