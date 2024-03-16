b3c6ab4ebfff11f3b4ef10f5ea9b04b53f38c350
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 21:18:58 +0000

    god of pain

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,17 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * array_range - entry
+ * @min: min value
+ * @max: max value
+ * Description: ranges from min to max
+ * Return: int array
  */
 int *array_range(int min, int max)
 {
 	int *all;
-	int l = 0;
-
+	int l = 0, i = 0;
+	
 	if (min > max)
 		return (NULL);
-	all = malloc(sizeof(int) * l);
+
+	l = max - min + 1;
+	all = malloc(sizeof(int) * (l + 1));
 
 	if (all == NULL)
 		return (NULL);
-	
+	while (min <= max)
+	{
+		*(all + i) = min;
+		min++;
+		i++;
+	}
+	return (all);
 }
