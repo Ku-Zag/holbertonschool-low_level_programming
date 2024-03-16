256982934ef37d75956fb7d8a6f5459bc7294c18
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 20:12:24 +0000

    god of pain

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	int l1 = 0, l2 = 0, l3 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[l1] != 0)
+		l1++;
+	while (s2[l2] != 0)
+		l2++;
+	if (n >= l2)
+		n = l2;
+	s3 = malloc(sizeof(char) * (l1 + n + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+	while (*s1)
+	{
+		s3[l3] = *s1;
+		l3++;
+		s1++;
+	}
+}
