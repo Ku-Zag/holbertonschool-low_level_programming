cae1f07d0ff0b63a61e1a6b1b1f4338df925ae6c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 19:09:01 +0000

    god of pain

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * malloc_checked - entru
  * @b: memory size
@@ -13,6 +12,6 @@ void *malloc_checked(unsigned int b)
 	ptr = malloc(b);
 
 	if (ptr == NULL)
-		return(NULL);
+	       exit (98);
 	return (ptr);
 }
