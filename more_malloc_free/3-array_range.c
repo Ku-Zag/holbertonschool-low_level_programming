b1efb40795e1831aefc0bfd3556c5ff158a31901
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-16 21:10:01 +0000

    god of pain

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ */
+int *array_range(int min, int max)
+{
+	int *all;
+	int l = 0;
+
+	if (min > max)
+		return (NULL);
+	all = malloc(sizeof(int) * l);
+
+	if (all == NULL)
+		return (NULL);
+	
+}
