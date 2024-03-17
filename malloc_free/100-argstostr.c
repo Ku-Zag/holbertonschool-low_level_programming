eccbff51ba3175ac78e6df6cfa0fa54e6367a860
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-17 18:17:57 +0000

    mal lock

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ */
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int na = 0;
+	int l = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (; na < ac; na++)
+	{
+		ZZ
+		av[l]
+	}
+	ptr = (char**)malloc(sizeof(char *) * l);
+	return (ptr);
+}
