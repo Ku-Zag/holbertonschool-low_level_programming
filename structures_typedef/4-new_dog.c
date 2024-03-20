169483d4932ddded329daec01517c5aafe8f4d52
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-20 19:14:24 +0000

    everybody knows

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,14 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * new_dog - entry
+ * @name: name of dog
+ * @age: GE
+ * @owner: slaver
+ * Description: five night of frefddy
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *nnanme;
-	char *nowner;
 	int l1 = 0;
 	int l2 = 0;
-	int i = 0;
+	int i;
 	dog_t *new_dog;
 
 	while (name[l1])
@@ -24,8 +27,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog->name == NULL)
 		return (NULL);
+	for (i = 0; i < l1; i++)
+		new_dog->name[i] = name[i];
 
 	new_dog->age = age;
 
-	new_dog->owner = malloc(sizeof(char) * )
+	new_dog->owner = malloc(sizeof(char) * l2);
+
+	if (new_dog->owner == NULL)
+		return(NULL);
+	for (i = 0; i < l2; i++)
+		new_dog->owner[i] = owner[i];
+	return (new_dog);
 }
