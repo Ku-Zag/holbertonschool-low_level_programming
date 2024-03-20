b20dc741f54b44a54c9a151f899e721f3ed443f4
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-20 17:55:44 +0000

    everybody knows

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *nnanme;
+	char *nowner;
+	int l1 = 0;
+	int l2 = 0;
+	int i = 0;
+
+	while (name[l1])
+		l1++;
+	while (owner[l2])
+		l2++;
+
+}
