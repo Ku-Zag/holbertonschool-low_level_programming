fb43c445decf8bf9cea018803b90ac310cdbd7e1
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 13:29:53 +0000

    VV

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * print_name - prints naem
+ * @name: vincent
+ * @f: pointer
+ * Description: prints name
  */
 void print_name(char *name, void (*f)(char *))
 {
