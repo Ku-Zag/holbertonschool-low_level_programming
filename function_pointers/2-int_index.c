759a06ed84532196a3072a04c250604ef4bdeb59
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 16:37:09 +0000

    VV

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  */
-int int_index(int array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
@@ -9,6 +9,9 @@ int int_index(int array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+		if (cmp == array[i])
+			return (i);
 	}
+	if (i == size)
+		return (-1);
 }
