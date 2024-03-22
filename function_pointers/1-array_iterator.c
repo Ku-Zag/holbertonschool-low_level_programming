c0b1a6033a17745a80b4b39e3c03dda0e26b294b
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 15:08:49 +0000

    VV

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,13 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * array_iterator - entry
+ * @array: array
+ * @size: size of array
+ * @action: pointer to func
+ * Description: returns pointer to func with int value from array
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
