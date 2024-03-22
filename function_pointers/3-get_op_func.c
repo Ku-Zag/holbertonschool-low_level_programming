3222a2f6ac4365be7e8d3c4f2623477143677577
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 19:51:44 +0000

    VV

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+/**
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+}
