70b9034e1d4242fa6509b7109e532d32c2ecf56d
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 18:43:17 +0000

    VV

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * int_index - entry
  * @size: size of array
@@ -10,8 +11,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
