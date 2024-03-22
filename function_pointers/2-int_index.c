20830049c95d8dc7794737a74a38c0cdfadbad4b
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 18:05:44 +0000

    VV

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,9 +9,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp == array[i])
+		if ((*cmp)(i+1) == array[i])
 			return (i);
 	}
 	if (i == size)
 		return (-1);
+	return (0);
 }
