1abbc88c58a4f0298ffd1b5bad686aae3142e402
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 20:31:42 +0000

    VV

---
+++ function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: arg c
+ * @argv: string
+ * Description: gives argment
+ * Return: integer
+ */
+int main (int argc, char *argv[])
+{
+
+}
