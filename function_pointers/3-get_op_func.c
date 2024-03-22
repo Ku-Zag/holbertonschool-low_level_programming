f65c6cc008c18665cae0455a1f4a0a860bd1d167
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-22 21:53:30 +0000

    VV

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -4,7 +4,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+	int i = 0;
 	op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
@@ -14,10 +14,11 @@ int (*get_op_func(char *s))(int, int)
 	{NULL, NULL}
 	};
 
-	for  (i = 0; i < 5; i++)
+	while (i < 5)
 	{
 		if (*(ops[i].op) == *s && *(s + 1) == 0)
 			return (ops[i].f);
+		i++;
 	}
 	return (0);
 }
