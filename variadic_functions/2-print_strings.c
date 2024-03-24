80ba9944bd4e37e6e507be19620f888058425481
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-24 17:55:40 +0000

    ziber

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - entry
+ * @separator: separaates
+ * @n: n of a
+ * Description: pritns all strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	va_
+}
