6f4f160b63eeb3f185d912fbb4de8592cb4a528c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-24 19:32:50 +0000

    ziber

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,7 +3,65 @@
 #include <stdarg.h>
 /**
  */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, float));
+}
+
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, char));
+}
+
+void print_string(va_list args)
+{
+	char *s;
+	s = va_arg(args, char *);
+
+	if (*s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
 void print_all(const char * const format, ...)
 {
-	
+	types list[] = 
+	{
+		{"i", print_int},
+		{"f", print_float},
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list args;
+	int i = 0;
+	int j = 0;
+	char *separator = "";
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		while (list[j].type)
+		{
+			if (list[j].type == format[i])
+			{
+				printf("%s", *separator);
+				list[j].f(args);
+				separator = ", ";
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
