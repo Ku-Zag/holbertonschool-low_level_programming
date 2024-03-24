56e4396a1aca6afca409f968c10631c8db84714f
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-24 20:07:43 +0000

    ziber

--- variadic_functions/a.h
+++ variadic_functions/a.h
@@ -1,4 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-void p_all()
+void p_all(const char * const format, ...);
+typedef struct a
+{
+	char *type;
+	void (*f)(args);
+}all_types;
+void p_int(va_list args);
+void p_char(va_list args);
+void p_flt(va_list args);
+void p_str(va_list args);
