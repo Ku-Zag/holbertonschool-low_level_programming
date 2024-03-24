41504f15ec6f4c6436e61192cc704938a6d8b1b1
Ku-Zag <abdullamcidov2003@gmail.com>
2024-03-24 19:44:36 +0000

    ziber

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,22 +2,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * print_int - entr
+ * @args: argument
+ * Description: prints
  */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
-
+/**
+ * print_float - entr
+ * @args: argument
+ * Description: prints
+ */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
-
+/**
+ * print_char - entr
+ * @args: argument
+ * Description: prints
+ */
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
-
+/**
+ * print_string - entr
+ * @args: argument
+ * Description: prints
+ */
 void print_string(va_list args)
 {
 	char *s;
@@ -31,7 +46,11 @@ void print_string(va_list args)
 	}
 	printf("%s", s);
 }
-
+/**
+ * print_all - entr
+ * @format: types of args
+ * Description: prints
+ */
 void print_all(const char * const format, ...)
 {
 	types list[] = {
