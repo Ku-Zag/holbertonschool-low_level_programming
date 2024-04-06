b386a46ea5b49f69a5b67e8aaacdd1144d137558
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-06 23:50:36 +0000

    double

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 /**
+ * add_dnodeint - adds node to start
+ * @head: head, @n: integer
+ * Description: intger in nodes
+ * Return: adress of new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
