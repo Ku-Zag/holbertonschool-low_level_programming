ee3959e6d64d6d05fb71acde76f1b2c0fe577f9d
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-06 17:06:43 +0000

    double

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,10 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *current = h;
 	size_t i = 0;
 
-	while (h)
+	while (current)
 	{
-		printf("%n\n", current->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
 		i++;
 	}
 	return (i);
