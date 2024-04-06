218e8530babf918ba1bb359d001a6bc3dff6884c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-06 13:16:23 +0000

    single

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - enrtr
+ * @h: head
+ * Description: head
+ * Return: number of elemnts
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+
+	while (h)
+	{
+		if (h->str)
+			i++;
+		else
+			return(i);
+		h = h->next;
+	}
+	return (i);
+}
