937c58c15e098bea5c95d9dcc3fada8bbac5a80f
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-06 12:43:42 +0000

    single

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,5 +3,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	unsigned int i = 0;
+	const list_t *current = h;
 
+	while (current)
+	{
+		if (current->str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		current = current->next;
+		i++;
+	}
+	return (i);
 }
