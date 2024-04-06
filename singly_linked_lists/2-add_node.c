f99519f94959066deec79bfe1588168708906a85
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-06 13:58:04 +0000

    single

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,9 +8,13 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
 	unsigned int i = 0;
 
+	new_node =  malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
 	while (str[i])
 		i++;
 	new_node->len = i;
