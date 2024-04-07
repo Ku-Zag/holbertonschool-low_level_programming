88c9a79a8eab1b6096730367fb3c13569ed7ea40
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 04:31:41 +0000

    double

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *del, *cur = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (idx == 0)
+	{
+		*head = cur->next;
+		(*head)->prev = NULL;
+		return (1);
+	}
+	while (cur && i < index)
+	{
+		del = cur;
+		cur = cur->next;
+		i++;
+	}
+	if (cur == NULL)
+		return (-1);
+	del->next = cur->next;
+	if (cur->next)
+		cur->next->prev = del;
+	return (1);	
+}
