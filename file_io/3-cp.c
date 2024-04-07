1103e19a1d53726051e2620c153662e76527d7da
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 18:19:22 +0000

    mental ilness

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@ int main (int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n")
-		exit(97));
+		exit(97);
 	}
 	f01 = open(argv[1], O_RDONLY);
 
@@ -18,13 +18,42 @@ int main (int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f02 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNCAT, 0664);
+	f02 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (f02 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while 
+	while ((fc = read(f01, buffer, 1024) > 0))
+	{
+		if (fc < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		fc = read(f02, buffer, fc);
 
+		if (fc < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (fc < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(f01) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f01);
+		exit(100);
+	}
+	if(close(f02) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", f02);
+		exit(100);
+	}
+	return (0);
 }
