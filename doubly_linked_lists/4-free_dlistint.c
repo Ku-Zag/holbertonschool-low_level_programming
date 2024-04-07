8bc8f6b34d0d2a7d476e4e801b470b4d06df28c6
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 01:13:13 +0000

    double

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+	free(head);
+}
