0a1f74b05efd29e0b92fb97aed8f9cf4fee39b33
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 18:30:01 +0000

    mental ilness

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,59 +1,54 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
+ * ain - Copies content of file to another
+ *
+ * @argc: int
+ *
+ * @argv: double pointer
+ *
+ * Return: Copy of file
  */
-int main (int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	int f01, f02, fc;
+	int fd, fd2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	f01 = open(argv[1], O_RDONLY);
-
-	if (f01 < 0)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f02 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (f02 < 0)
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((fc = read(f01, buffer, 1024) > 0))
-	{
-		if (fc < 0)
+		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		fc = read(f02, buffer, fc);
-
-		if (fc < 0)
+		filecheck = write(fd2, buffer, filecheck);
+		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (fc < 0)
+	if (filecheck == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(f01) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f01);
-		exit(100);
-	}
-	if(close(f02) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", f02);
-		exit(100);
-	}
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
 	return (0);
 }
