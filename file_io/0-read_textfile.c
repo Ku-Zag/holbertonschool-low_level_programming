f5c7dd059cfb3fe0e983742245ee3286e02b321c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 16:52:04 +0000

    mental ilness

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,11 +7,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int o, r, w;
 
 	if (filename == NULL)
-		return(NULL);
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
-		return (NULL);
+		return (0);
 	o = open(filename, O_RDONLY);
 
 	if (o < 0)
@@ -32,5 +32,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (w < 0)
 		return (0);
-	return (ssize_t w);
+	return ((ssize_t)w);
 }
