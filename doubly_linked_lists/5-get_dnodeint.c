6e75db1ad610fe6452a2dd2dd854b101bbed2569
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 01:29:35 +0000

    double

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,14 @@
 #include "lists.h"
+#include <limits.h>
 /**
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+	unsigned int max = UINT_MAX;
 
+	if (index > max)
+		return (NULL);
 	while (i < index)
 	{
 		head = head->next;
