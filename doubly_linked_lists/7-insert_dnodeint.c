af6ffce512e45a81421a0e595585423c5c74f5f4
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 03:45:14 +0000

    double

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new;
+	dlistint_t *cur = *h;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+
+	while (cur && i < idx - 1)
+	{
+		cur = cur->next;
+		i++;
+	}
+	new->prev = cur;
+	new->next = cur->next;
+
+	if (cur->next)
+		cur->next->prev = new;
+	cur->next = new;
+	return (new);
+}
