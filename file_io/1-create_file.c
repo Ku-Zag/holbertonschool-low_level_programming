53cc2d15eb8a9e3e5a8fa65b2a1f63d66f8c2f7a
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 17:28:30 +0000

    mental ilness

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * create_file - entry
+ * @filename: name of file
+ * @text_content: context of text
+ * Description: boring
+ * Return: int
  */
 int create_file(const char *filename, char *text_content)
 {
