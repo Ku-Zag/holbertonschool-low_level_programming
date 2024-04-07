6d7502f47b9149e74f976fff9dcef97e9502ef3c
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 01:31:24 +0000

    double

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	unsigned int max = UINT_MAX;
 
-	if (index > max)
+	if (index >= max)
 		return (NULL);
 	while (i < index)
 	{
