530dcf073ba27a39b37d287bad551cb0ff38a563
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 03:51:43 +0000

    double

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,10 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	while (len)
 	{
-		len = len->next
+		len = len->next;
 		l++;
 	}
-	if (index > l)
+	if (idx > l)
 		return (NULL);
 	new->n = n;
 
