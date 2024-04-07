6abe951038a669dc7c224dbc08ff26a5c82f898d
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-07 11:06:40 +0000

    bitlen

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,23 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 0, test = 32768;
-	if (n > test)
-		return;
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (test)
-	{
-		if (i == 1 && (n & test) == 0)
-			_putchar('0');
-		else if ((n & test) != 0)
-		{
-			_putchar('1');
-			i = 1;
-		}
-		test >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
